30fe290d3693168541c5dec8f0e688937b5c5bd4
kimkunndad <kim.kundad@gmail.com>
2022-10-03 12:59:36 +0700

    kim update sms kbank

---
+++ components/data/contact.js
@@ -0,0 +1,27 @@
+export default [
+    {
+      date: "24/05/65 13:35:40",
+      name: "สหรัฐ เจริญมี",
+      sender: 'SMSPANG',
+      status: 'success'
+    },
+    {
+        date: "24/05/65 13:35:40",
+        name: "สหรัฐ เจริญมี",
+        sender: 'SMSPANG',
+        status: 'reject'
+      },
+      {
+        date: "24/05/65 13:35:40",
+        name: "สหรัฐ เจริญมี",
+        sender: 'SMSPANG',
+        status: 'pending'
+      },
+      {
+        date: "24/05/65 13:35:40",
+        name: "สหรัฐ เจริญมี",
+        sender: 'SMSPANG',
+        status: 'success'
+      },
+  ];
+  
